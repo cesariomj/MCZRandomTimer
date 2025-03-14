248cd9091b798ebcfa9d070102af77e4026e07c6
mcesario <mcesario@pm.me>
2025-03-14 13:22:07 -0600

    Initial Commit

---
+++ RandomTimer/RandomTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTimerApp.swift
+//  RandomTimer
+//
+//  Created by M1Chip MacBook Pro on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct RandomTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
